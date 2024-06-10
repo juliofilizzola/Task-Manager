cf33e3bc0768e1b73765b2270b6a09744f705bbd
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-10 18:46:55 -0300

    create config db

---
+++ Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace Infra.Data.Context;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options) {
+
+    public DbSet<TodoTask> TodoTasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMongoDB("mongodb://localhost:27017", "YourDatabaseName");
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+    
+}
---
+++ Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace Infra.IoC;
+
+public class DependencyInjection {
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseMongoDB(configuration.GetConnectionString("DefaultConnection"), "Task-Manager"));
+
+
+        var applicationAssembly = AppDomain.CurrentDomain.Load("Task-Manager");
+
+        return services;
+    }
+}

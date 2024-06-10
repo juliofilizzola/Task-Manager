faae7c96e4a00bf3d79d29a6911d3477c074caea
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-10 17:27:12 -0300

    Create Interface domain todo Task

---
+++ Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entity;
+
+namespace Domain.Interfaces;
+
+public interface ITaskRepository {
+    Task<IEnumerable<TodoTask>> GetTasksAsync();
+    Task<TodoTask> GetTaskByIdAsync(int? id);
+    Task<TodoTask> CreateAsync(TodoTask todoTask);
+    Task<TodoTask> UpdateAsync(TodoTask todoTask);
+    Task<TodoTask> RemoveAsync(TodoTask todoTask);
+}

31f35f632ffbcfac9e32e756466df64324ff2934
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-10 16:50:27 -0300

    create Domain

---
+++ Domain/Entity/Task.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Utils;
+using Domain.Validation;
+
+namespace Domain.Entity;
+
+public class Task : Base {
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public string? SubName { get; set; }
+    public bool IsComplete { get; set; } = false;
+    private int PercentageCompleted { get; set; } = 0;
+
+
+    public Task(string name, string? subName, int percentageCompleted) {
+        ValidateDomain(name);
+        Name = name;
+        SubName = subName;
+        PercentageCompleted = ValidatePercentage(percentage: percentageCompleted);
+        Code = Ramdom.RandomStringCode(6);
+        if (PercentageCompleted == 100){
+            IsComplete = true;
+        }
+    }
+
+    public void Update(string name, string code, string? subName, int percentageCompleted) {
+        ValidateDomain(name);
+        Name = name;
+        SubName = subName;
+        Code = code;
+        PercentageCompleted = percentageCompleted;
+    }
+
+    private void ValidateDomain(string name) {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid, Name is required");
+        DomainExceptionValidation.When(name.Length < 3, "Invalid name, to short, minimum 3 characters");
+    }
+
+    private int ValidatePercentage(int percentage) {
+        return percentage switch {
+            < 0   => 0,
+            > 100 => 100,
+            _     => percentage
+        };
+
+    }
+}
---
+++ Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Validation;
+
+public class DomainExceptionValidation(string err) : Exception(err) {
+    public static void When(bool hasError, string error) {
+        if (hasError){
+            throw new DomainExceptionValidation(error);
+        }
+    }
+}

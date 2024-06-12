8fc523bc209367c7296b12edd397217381bb6770
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-12 16:03:28 -0300

    create context application

---
+++ Infra-data/Context/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra_data.Context;
+
+public class ApplicationContext : DbContext {
+    public DbSet<TodoTask> TodoTasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder opt) {
+        opt.UseNpgsql("");
+    }
+}

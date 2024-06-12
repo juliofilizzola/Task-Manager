ba2d75291ae4ccc69540cff19018fe15c36f39ab
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-12 15:51:41 -0300

    remove solution

--- Infra.Data/Program.cs
+++
@@ -1,7 +0,0 @@
-using Infra.Data;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
--- Infra.IoC/DependencyInjection.cs
+++ Infra.IoC/DependencyInjection.cs
@@ -1,17 +1,15 @@
 ﻿using Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
-using MongoDB.EntityFrameworkCore.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infra.IoC;
 
-public class DependencyInjection {
+public static class DependencyInjection {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseMongoDB(configuration.GetConnectionString("DefaultConnection"), "Task-Manager"));
-
-
-        var applicationAssembly = AppDomain.CurrentDomain.Load("Task-Manager");
-
+            options.UseMongoDB(configuration.GetConnectionString("DefaultConnection") ?? string.Empty, "Task-Manager"));
+        
         return services;
     }
 }

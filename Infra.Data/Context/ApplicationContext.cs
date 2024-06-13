579002acc10c1145e232b2f312c75bf973c3b0f3
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-13 13:43:14 -0300

    modify Context and create taskRepository

--- Infra.Data/Context/ApplicationContext.cs
+++ Infra.Data/Context/ApplicationContext.cs
@@ -7,7 +7,7 @@ public class ApplicationContext : DbContext {
     public DbSet<TodoTask> TodoTasks { get; set; }
     public ApplicationContext(DbContextOptions<ApplicationContext> op) : base(op) {}
     protected override void OnConfiguring(DbContextOptionsBuilder opt) {
-        opt.UseNpgsql("");
+        opt.UseNpgsql("Host=localhost;Port=5433;Database=task_manager;Username=docker;Password=123456;");
     }
     protected override void OnModelCreating(ModelBuilder builder) {
         base.OnModelCreating(builder);
---
+++ Infra.Data/Repository/TodoTaskRepository.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Interfaces;
+using Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Repository;
+
+public class TodoTaskRepository : ITaskRepository {
+    private readonly ApplicationContext _context;
+
+    public TodoTaskRepository(ApplicationContext context) {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Domain.Entity.TodoTask>> GetTasksAsync() {
+        var todos = await _context.TodoTasks.ToListAsync();
+
+        return todos;
+    }
+
+    public async Task<Domain.Entity.TodoTask> GetTaskByIdAsync(Int32? id) {
+        var todos = await _context.TodoTasks.FindAsync(id);
+
+        if (todos == null){
+            throw new NullReferenceException();
+        }
+
+        return todos;
+    }
+
+    public async Task<Domain.Entity.TodoTask> CreateAsync(Domain.Entity.TodoTask todoTask) {
+        _context.TodoTasks.Add(todoTask);
+        await _context.SaveChangesAsync();
+        return todoTask;
+    }
+
+    public async Task<Domain.Entity.TodoTask> UpdateAsync(Domain.Entity.TodoTask todoTask) {
+        _context.TodoTasks.Update(todoTask);
+        await _context.SaveChangesAsync();
+        return todoTask;
+    }
+
+    public async Task<Domain.Entity.TodoTask> RemoveAsync(Domain.Entity.TodoTask todoTask) {
+        _context.TodoTasks.Remove(todoTask);
+        await _context.SaveChangesAsync();
+        return todoTask;
+    }
+}

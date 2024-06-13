9f09fa15444478063ec89ed8d90632e69d567fe4
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-13 13:39:59 -0300

    create migration

---
+++ Infra.Data/Migrations/20240613110649_Initial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoTasks",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Code = table.Column<string>(type: "character varying(6)", maxLength: 6, nullable: false),
+                    SubName = table.Column<string>(type: "text", nullable: true),
+                    IsComplete = table.Column<bool>(type: "boolean", nullable: false),
+                    PercentageCompleted = table.Column<int>(type: "integer", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoTasks", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoTasks",
+                columns: new[] { "Id", "Code", "CreateAt", "IsComplete", "Name", "PercentageCompleted", "SubName" },
+                values: new object[] { "", "82SdPz", new DateTime(2024, 6, 13, 11, 6, 49, 89, DateTimeKind.Utc).AddTicks(5561), false, "Task Test", 0, "" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoTasks");
+        }
+    }
+}
---
+++ Infra.Data/Migrations/20240613161624_Description.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Description : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TodoTasks",
+                keyColumn: "Id",
+                keyValue: "");
+
+            migrationBuilder.RenameColumn(
+                name: "SubName",
+                table: "TodoTasks",
+                newName: "Description");
+
+            migrationBuilder.InsertData(
+                table: "TodoTasks",
+                columns: new[] { "Id", "Code", "CreateAt", "Description", "IsComplete", "Name", "PercentageCompleted" },
+                values: new object[] { "orqGEQkjZ4LVcmxxXLvKrXkKNrTNW24rzH", "1RSjwX", new DateTime(2024, 6, 13, 16, 16, 24, 282, DateTimeKind.Utc).AddTicks(2791), "", false, "Task Test", 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TodoTasks",
+                keyColumn: "Id",
+                keyValue: "orqGEQkjZ4LVcmxxXLvKrXkKNrTNW24rzH");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "TodoTasks",
+                newName: "SubName");
+
+            migrationBuilder.InsertData(
+                table: "TodoTasks",
+                columns: new[] { "Id", "Code", "CreateAt", "IsComplete", "Name", "PercentageCompleted", "SubName" },
+                values: new object[] { "", "82SdPz", new DateTime(2024, 6, 13, 11, 6, 49, 89, DateTimeKind.Utc).AddTicks(5561), false, "Task Test", 0, "" });
+        }
+    }
+}

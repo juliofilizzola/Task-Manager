b5f00f3bb4e73c9f12d8cda57d9cd52aed0e1fc4
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-15 10:52:15 -0300

    add middleware exception

---
+++ Application/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Application.Exceptions;
+
+namespace Application.Middlewares;
+
+public class ExceptionHandlingMiddleware {
+
+    private readonly RequestDelegate _next;
+    public ExceptionHandlingMiddleware(RequestDelegate next) {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context) {
+        try {
+            await _next(context);
+        } catch (Exception e) {
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+
+        context.Response.StatusCode = exception is NotFoundException ? StatusCodes.Status404NotFound : StatusCodes.Status500InternalServerError;
+
+        var result = new { message = exception.Message };
+        return context.Response.WriteAsJsonAsync(result);
+    }
+}

65be910f645421497975bb2430fc4cd13494b7d9
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-15 10:21:28 -0300

    Add new mapping

--- Application/Mappings/DomainToDtoMappingProfile.cs
+++ Application/Mappings/DomainToDtoMappingProfile.cs
@@ -7,5 +7,8 @@ namespace Application.Mappings;
 public class DomainToDtoMappingProfile : Profile{
     public DomainToDtoMappingProfile() {
         CreateMap<TodoTask, TodoTaskDto>().ReverseMap();
+        CreateMap<TodoTaskDto, TodoTask>().ReverseMap();
+        CreateMap<UpdateTodoTaskDto, TodoTask>().ReverseMap();
+        CreateMap<UpdateTodoTaskDto, TodoTask>().ReverseMap();
     }
 }

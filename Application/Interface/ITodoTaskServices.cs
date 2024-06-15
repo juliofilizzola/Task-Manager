cf07c2e41db239a777a2b08710c0ce20f159892c
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-15 10:21:53 -0300

    add update progress

--- Application/Interface/ITodoTaskServices.cs
+++ Application/Interface/ITodoTaskServices.cs
@@ -4,8 +4,9 @@ namespace Application.Interface;
 
 public interface ITodoTaskServices {
     Task<IEnumerable<TodoTaskDto>> GetTodoTask();
-    Task <TodoTaskDto> GetTodoTasksByID(string? id);
+    Task <TodoTaskDto> GetTodoTasksById(string? id);
     Task <TodoTaskDto> Add(TodoTaskDto todoTaskDto);
-    Task<TodoTaskDto> Update(TodoTaskDto todoTaskDto, string id);
+    Task<TodoTaskDto> Update(UpdateTodoTaskDto todoTaskDto, string id);
+    Task<TodoTaskDto> UpdateProgress(string id, int? progress);
     Task<bool> Remove(string? id);
 }

22f160de4ff2c2009647b9ca8aad58726f90a329
julio_filizzola <juliofilizzola@hotmail.com>
2024-07-08 12:25:26 -0300

    Remove hardcoded database connection string from ApplicationContext

--- Infra.Data/Context/ApplicationContext.cs
+++ Infra.Data/Context/ApplicationContext.cs
@@ -4,11 +4,8 @@ using Microsoft.EntityFrameworkCore;
 namespace Infra.Data.Context;
 
 public class ApplicationContext : DbContext {
-    public DbSet<TodoTask> TodoTasks { get; set; }
     public ApplicationContext(DbContextOptions<ApplicationContext> op) : base(op) {}
-    protected override void OnConfiguring(DbContextOptionsBuilder opt) {
-        opt.UseNpgsql("Host=task-manager-db-prod-juliofilizzola.g.aivencloud.com;Database=defaultdb;Port=26577;Username=avnadmin;Password=AVNS_He4E4_I_vr86bz9ZVJQ;");
-    }
+    public DbSet<TodoTask> TodoTasks { get; set; }
     protected override void OnModelCreating(ModelBuilder builder) {
         base.OnModelCreating(builder);
         builder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
